ecfac8909fa92fc571184986ac84c79665c5582c
moliveri1618 <lastiada1@gmail.com>
2024-07-13 18:16:57 +0100

    comi

---
+++ TD_MatchPDF_backend_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TdMatchpdfBackendProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TD_MatchPDF_backend_project'
