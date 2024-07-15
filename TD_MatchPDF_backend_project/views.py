d7c4acabf363f48f9c2a5e78cd38d732b9c2cc4a
moliveri1618 <lastiada1@gmail.com>
2024-07-15 16:13:36 +0100

    commit

--- TD_MatchPDF_backend_project/views.py
+++ TD_MatchPDF_backend_project/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from django.core.files.storage import FileSystemStorage
 import os
+import shutil
+
 
 # Create your views here.
 @api_view(['POST'])
@@ -10,22 +12,35 @@ def yo(request):
     return HttpResponse("hahahaha")
 
 
-# Create your views here.
 @api_view(['POST'])
 def pdf_compare(request):
-    if 'file' in request.FILES:
-        uploaded_file = request.FILES['file']
-        print('yoyo')
-        print(f'Filename: {uploaded_file.name}')
-        script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Save the uploaded file to the Files directory
-        fs = FileSystemStorage(location=script_dir)
-        filename = fs.save(uploaded_file.name, uploaded_file)
-        file_path = fs.path(filename)
-        print(f'File saved at: {file_path}')
+    if 'file1' in request.FILES and 'file2' in request.FILES:
+        uploaded_file1 = request.FILES['file1']
+        uploaded_file2 = request.FILES['file2']
+
+        # Define the directory to store the files
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        files_dir = os.path.join(script_dir, 'Files')
+
+        # Delete all existing files in the directory
+        if os.path.exists(files_dir):
+            shutil.rmtree(files_dir)
+        os.makedirs(files_dir)
+
+        # Save the uploaded files to the Files directory
+        fs = FileSystemStorage(location=files_dir)
+        filename1 = fs.save(uploaded_file1.name, uploaded_file1)
+        filename2 = fs.save(uploaded_file2.name, uploaded_file2)
+        file_path1 = fs.path(filename1)
+        file_path2 = fs.path(filename2)
+        print(f'File 1 saved at: {file_path1}')
+        print(f'File 2 saved at: {file_path2}')
+
+        # Here you can add your logic to compare the PDF files
+        # For example, you could use a library like PyPDF2 or pdfminer to extract text and compare
 
     else:
-        print('No file uploaded')
-    
-    return HttpResponse("yuppiii")
+        print('Both files are required')
+
+    return HttpResponse("Files received and processed")
