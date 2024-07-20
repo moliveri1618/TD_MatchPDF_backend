b800abfc62acedeae0e733aa08a76a15fb1bf024
moliveri1618 <lastiada1@gmail.com>
2024-07-21 00:09:11 +0100

    commit

--- TD_MatchPDF_backend_project/views.py
+++ TD_MatchPDF_backend_project/views.py
@@ -9,13 +9,19 @@ from django.http import JsonResponse
 def yo(request):
     return HttpResponse("yuuuu")
 
-#all_ai_matches = []
+all_ai_matches = []
 @api_view(['POST'])
 def pdf_compare_ordine_e_conferma(request):
+    global all_ai_matches
 
+    # Get data from session
+    isPageReloaded = request.data.get('isPageReloaded', '')
+    if isPageReloaded == 'true': all_ai_matches = []
+    
     nuova_regola = request.data.get('regole', '')
     nuova_regola_list = nuova_regola_safe_check(nuova_regola)
-    #print(nuova_regola_list)
+    # print(nuova_regola_list)
+    # print(isPageReloaded)
 
     if 'file1' in request.FILES and 'file2' in request.FILES:
 
@@ -32,9 +38,9 @@ def pdf_compare_ordine_e_conferma(request):
             pos_client_senza_match1, pos_client_senza_match2 = remove_resul12_from_array_senza_match(pos_client_senza_match1, pos_client_senza_match2, result1_pos_cliente, result2_pos_cliente)
 
             if res_AI != '':
-                # all_ai_matches.append(res_AI.copy())
-                # for item in all_ai_matches: res = append_2_dict(res, item)
-                append_2_dict(res, res_AI)
+                all_ai_matches.append(res_AI.copy())
+                for item in all_ai_matches: res = append_2_dict(res, item)
+                #append_2_dict(res, res_AI)
                 # print(res)
                 # print(all_ai_matches[0])
                 # print(res_AI)
